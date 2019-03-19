40dd659d0566d25154727980c9c83215291ecd0f
navarrenicolas <navarrenicolas@gmail.com>
2019-03-18 19:15:26 -0700

    adding all code

--- generateData.m
+++ generateData.m
@@ -5,22 +5,31 @@
 %%
 v_rel_in = 20*rand; % relative wind speed from 0 to 20 m/s
 w_rel_in = rand*2*pi-pi; %wind angle angle from -pi to pi rad
-yaw_in = rand*2*pi-pi; % heading angle from -pi to pi rad
-ddyaw_in = rand*pi/2-pi/4; %heading angular acceleration from -pi/4 to pi/4 rad/s^2
-roll_in = rand*pi-pi/2; %heel angle from -pi/2 to pi/2
-ddroll_in = rand*pi/2-pi/4; %heel angular acceleration rad/s^2
+psi_in = rand*2*pi-pi; % heading angle from -pi to pi rad
+phi_in = rand*pi-pi/2; %heel angle from -pi/2 to pi/2
 x_in = rand*1000; %(gps) x-position from 0 to 1000 m
 dx_in = rand*40-20; %x-velocity from -20 to 20 m/s
 y_in = rand*1000; %(gps) y-position from 0 to 1000 m
 dy_in = rand*40-20; %y-velocity from -20 to 20 m/s
-du_in = rand*10-5; %forward acceleration from -5 to 5 m/s^2
-dv_in = rand*10-5; %sideways acceleraton from -5 to 5 m/s^2
 rangle_in = rand*2*pi/3-pi/3; %rudder angle from -pi/3 to pi/3
+roll_in = rand*2*pi-pi;
+yaw_in = rand*2*pi-pi;
+
 if w_rel_in > 0
     sangle_in = rand*pi/2;
 else
     sangle_in = -rand*pi/2;
 end
+boat_speed_in = sqrt(dy_in^2 + dx_in^2);
 
+
+% True wind angle and velocity
+v_true = sqrt(v_rel_in^2 + boat_speed_in^2 - 2*v_rel_in*boat_speed_in*cos(w_rel_in));
+
+if w_rel_in > 0 
+    w_true = acos((v_rel_in*cos(w_rel_in) - boat_speed_in)/v_true)+psi_in;
+else
+    w_true = -acos((v_rel_in*cos(w_rel_in) - boat_speed_in)/v_true)+psi_in;
+end
 %%
 save('inputData');
---
+++ ss_linearization.m
@@ -0,0 +1,30 @@
+load('inputData.mat');
+load('equations.mat');
+
+n_in = [x_in,y_in,phi_in,psi_in];
+v_in = transpose(eval(subs(J,[psi,phi],[psi_in,phi_in])\[dx_in;dy_in;roll_in;yaw_in]));
+ang_in = [sangle_in,rangle_in];
+
+linearized_A = jacobian(ss_eq,ss_vars);
+linearized_B = jacobian(ss_eq,ss_in);
+
+linearized_A = subs(linearized_A,[ss_vars, ss_in], [n_in, v_in, ang_in]);
+linearized_B = subs(linearized_B,[ss_vars, ss_in], [n_in, v_in, ang_in]);
+
+C = [1 0 0 0 0 0 0 0;
+    0 1 0 0 0 0 0 0;
+    0 0 1 0 0 0 0 0;
+    0 0 0 1 0 0 0 0];
+
+D = zeros(4,2);
+
+state_eq = ss(eval(real(linearized_A)), eval(real(linearized_B)), C, D);
+% Set the Inputs and Output names of the model with units
+state_eq.InputName = {'sail','rudder'};
+state_eq.InputUnit={'rad','rad'};
+state_eq.OutputName = {'dx','dy','dphi','dpsi'};
+state_eq.OutputUnit={'m/s','m/s','rad/s','rad/s'};
+
+%% Save the Model
+save state_eq.mat state_eq
+ 
