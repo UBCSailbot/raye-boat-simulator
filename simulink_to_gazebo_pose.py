9bda95dfc2c5570286b4973e84a36e162db7907b
Tyler Lum <tylergwlum@gmail.com>
2019-09-07 19:02:13 -0700

    Refactor python script to be more clear

--- simulink_to_gazebo_pose.py
+++ simulink_to_gazebo_pose.py
@@ -15,7 +15,7 @@ import numpy as np
 def tcp_sim():
 
     # Setup port and socket
-    sever_port=('127.0.0.1', 54320)
+    server_port=('127.0.0.1', 54320)
 
     try:
         # create an AF_INET, STREAM socket (TCP)
@@ -23,32 +23,20 @@ def tcp_sim():
     except:
         print('Failed to create a socket. ')
         sys.exit()
-    print('Socket Created :)')
-    sock.connect(sever_port)
 
-    # Starting client
+    print('Socket Created :)')
+    sock.connect(server_port)
     print("start a client")
     time.sleep(0.01)
+
+    # Send start command
     s="start"
     sock.send(s)
     print(s)
 
     state_msg = ModelState()
     state_msg.model_name = 'wamv'
-    state_msg.pose.position.x = 0
-    state_msg.pose.position.y = 0
-    state_msg.pose.position.z = 0.3
-    state_msg.pose.orientation.x = 0
-    state_msg.pose.orientation.y = 0
-    state_msg.pose.orientation.z = 0
-    state_msg.pose.orientation.w = 0
 
-    state_msg.twist.linear.x = 0
-    state_msg.twist.linear.y = 0
-    state_msg.twist.linear.z = 0
-    state_msg.twist.angular.x = 0
-    state_msg.twist.angular.y = 0
-    state_msg.twist.angular.z = 0
     while 1:
         # Receive data
         buf = sock.recv(1000)
@@ -59,22 +47,9 @@ def tcp_sim():
         buf_l = json.loads(buf)
         print("Loaded data:")
         print(buf_l)
-  
-        #state_msg.pose.position.z = buf_l[0]
-        #state_msg.pose.position.y = buf_l[1]
-        state_msg.pose.position.x = buf_l[0]
-        state_msg.pose.position.y = buf_l[1]
 
-        q = euler_to_quaternion(buf_l[2], 0, buf_l[3])
-        state_msg.pose.orientation.x = q[0]
-        state_msg.pose.orientation.y = q[1]
-        state_msg.pose.orientation.z = q[2]
-        state_msg.pose.orientation.w = q[3]
-
-        state_msg.twist.linear.x = buf_l[4]
-        state_msg.twist.linear.y = buf_l[5]
-        state_msg.twist.angular.x = buf_l[6]
-        state_msg.twist.angular.z = buf_l[7]
+        # Send gazebo control msg
+        state_msg = get_updated_model_state_msg(state_msg, buf_l)
 
         rospy.wait_for_service('/gazebo/set_model_state')
         try:
@@ -83,6 +58,7 @@ def tcp_sim():
         except rospy.ServiceException, e:
             print "Service call failed: %s" % e
 
+        # Send control signal back to simulink
         control_signal = 1
         s = str(control_signal)
         sock.send(s)
@@ -92,12 +68,28 @@ def tcp_sim():
 
 def euler_to_quaternion(roll, pitch, yaw):
 
-        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
-        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
-        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
 
-        return [qx, qy, qz, qw]
+    return [qx, qy, qz, qw]
+
+def get_updated_model_state_msg(msg, data):
+    msg.pose.position.x = data[0]
+    msg.pose.position.y = data[1]
+
+    q = euler_to_quaternion(data[2], 0, data[3])
+    msg.pose.orientation.x = q[0]
+    msg.pose.orientation.y = q[1]
+    msg.pose.orientation.z = q[2]
+    msg.pose.orientation.w = q[3]
+
+    msg.twist.linear.x = data[4]
+    msg.twist.linear.y = data[5]
+    msg.twist.angular.x = data[6]
+    msg.twist.angular.z = data[7]
+    return msg
 
 if __name__ == '__main__':
     rospy.init_node('set_pose')
