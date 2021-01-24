33891e1fc4f4c909713ce86ecb2571b72908853a
Joel Tsuchitori <TsuchitoriJoel@gmail.com>
2021-01-23 23:35:44 -1000

    Replaced the getHeadingError body with angdiff, refactored names in test

--- SailingYachtModel/HeadingErrorTest.m
+++ SailingYachtModel/HeadingErrorTest.m
@@ -1,18 +1,18 @@
 
-testRange(7 * pi / 8, -7*pi/8, -pi / 4, 0.1);
+testRange(7 * pi / 8, -7*pi/8, -pi / 4, 0.01);
 
-testRange(-7 * pi / 8, 7*pi/8, pi / 4, 0.1);
+testRange(-7 * pi / 8, 7*pi/8, pi / 4, 0.01);
 
-testRange(pi / 2, 0, pi / 2, 0.1);
+testRange(pi / 2, 0, pi / 2, 0.01);
 
-testRange(0, pi/2, -pi / 2, 0.1);
+testRange(0, pi/2, -pi / 2, 0.01);
 
-testRange(pi/4, -pi/4, pi/2, 0.1);
+testRange(pi/4, -pi/4, pi/2, 0.01);
 
-testRange(pi/3, -3 * pi/4, -11 * pi / 12, 0.1);
+testRange(pi/3, -3 * pi/4, -11 * pi / 12, 0.01);
 
 function testRange(setPoint, measure, target, range)
-    error = errorFun(setPoint, measure);
+    error = getHeadingError(setPoint, measure);
     assert(error > target - range && error < target + range);
 end
 
--- SailingYachtModel/getHeadingError.m
+++ SailingYachtModel/getHeadingError.m
@@ -1,14 +1,5 @@
 function error = getHeadingError(setPoint, measure)
 
-dist1 = setPoint - measure;
-dist2 = 2 * pi + dist1;
-if(dist2 > 2 * pi)
-   dist2 = -(2 * pi - dist1);
-end
+error = angdiff(measure, setPoint);
 
-if(abs(dist1) < abs(dist2))
-    error = dist1;
-else 
-    error = dist2;
 end
-
