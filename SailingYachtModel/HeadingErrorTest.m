ede15168989d87babce04c55a3cce651de2882c1
Joel Tsuchitori <TsuchitoriJoel@gmail.com>
2021-01-30 21:47:28 -1000

    Added test case for ranges outside of -2 Pi to 2 Pi

--- SailingYachtModel/HeadingErrorTest.m
+++ SailingYachtModel/HeadingErrorTest.m
@@ -11,6 +11,10 @@ testRange(pi/4, -pi/4, pi/2, 0.01);
 
 testRange(pi/3, -3 * pi/4, -11 * pi / 12, 0.01);
 
+testRange(4 * pi, -pi/4 + 2*pi, pi/4, 0.001);
+
+testRange(3 * pi, 1.1 * pi, -0.1 * pi, 0.001);
+
 function testRange(setPoint, measure, target, range)
     error = getHeadingError(setPoint, measure);
     assert(error > target - range && error < target + range);
