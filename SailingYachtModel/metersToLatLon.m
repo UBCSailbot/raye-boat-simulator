9fe5e52b440607bb2939139eae0006a19876123b
Joel Tsuchitori <TsuchitoriJoel@gmail.com>
2021-02-20 10:26:57 -1000

    Changed variable names to follow convention and changed comments

--- SailingYachtModel/metersToLatLon.m
+++ SailingYachtModel/metersToLatLon.m
@@ -1,9 +1,9 @@
-function [Lat, Lon] = metersToLatLon(x,y)
+function [lat, lon] = metersToLatLon(x,y)
     %Convert from UTM (meters) to lat and long
     %Coordinates are in meter starting in bottom left of squares which
     %divide earth into 60 x 60 grid
-    %convert is to convert to gps to coordinates
+    %returns GPS coordinates in decimal degrees
     utmZone = '17 T';
-    [Lat, Lon] = utm2deg(x, y, utmZone); 
+    [lat, lon] = utm2deg(x, y, utmZone); 
    
 end
