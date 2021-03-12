652778f6e0957b80fb3beb6ebbb44e60848ae6ad
bruce <brucec@hotmail.com>
2021-03-12 13:49:21 -0800

    Fix typo in previous commit's test about converting meters to GPS

--- SailingYachtModel/metersToLatLonTest.m
+++ SailingYachtModel/metersToLatLonTest.m
@@ -3,6 +3,6 @@ testFunction(630084,4833438, 43.6426,-79.3871);
 testFunction(530444 ,2833438 ,25.6182,-80.6968);
 
 function testFunction(x,y, expectedLat, expectedLon)
-    [lat, lon] = MetersToLatLon(x,y);
+    [lat, lon] = metersToLatLon(x,y);
      assert(round(lat,4) == expectedLat && round(lon, 4) == expectedLon);
 end 
