bc57f1953a541e2d0cce6e77bb0e1139b0d4ab35
bruce <brucec@hotmail.com>
2021-04-19 17:18:31 -0700

    add code to run Simulink model first before assertions so global variables load

--- SailingYachtModel/SimulinkControllerTest.m
+++ SailingYachtModel/SimulinkControllerTest.m
@@ -3,6 +3,11 @@
 %https://ubcsailbot.atlassian.net/browse/BSC-37
 
 MIN_ANGLE_DEVIATE_FROM_UP_WIND = pi/6;
+
+%run the simulation first so global variables defined
+%in the Simulink model loads
+ sim('SailingYachtModel'); 
+
 for desiredHeading = linspace(0,2*pi,20)
     % (par.alpha_w + pi) because wind blows from alpha_aw towards boat
     if isAngleBetween((par.alpha_w+pi)-MIN_ANGLE_DEVIATE_FROM_UP_WIND, ...
