52440dacf6c1996fb1fafca31afa482f71e81a81
Bruce Cui <34466348+brucecui97@users.noreply.github.com>
2021-04-28 18:50:09 -0700

    Delete error assertion from controller block, output error to workspace (#14)
    task: https://ubcsailbot.atlassian.net/browse/BSC-41

--- SailingYachtModel/SimulinkControllerTest.m
+++ SailingYachtModel/SimulinkControllerTest.m
@@ -3,11 +3,18 @@
 %https://ubcsailbot.atlassian.net/browse/BSC-37
 
 MIN_ANGLE_DEVIATE_FROM_UP_WIND = pi/6;
+
+%run the simulation first so global variables defined
+%in the Simulink model loads
+ sim('SailingYachtModel'); 
+
 for desiredHeading = linspace(0,2*pi,20)
     % (par.alpha_w + pi) because wind blows from alpha_aw towards boat
     if isAngleBetween((par.alpha_w+pi)-MIN_ANGLE_DEVIATE_FROM_UP_WIND, ...
             (par.alpha_w+pi) + MIN_ANGLE_DEVIATE_FROM_UP_WIND, desiredHeading)
         continue;
     end
+    disp("attempting to assert desiredheading = " + string(desiredHeading) + " radians is reached");
     sim('SailingYachtModel'); 
+    assert(abs(finalHeadingErrorFromSim)<0.05);
 end
