0cac48422a1d1cf94111a71508f56cb03f964645
bruce <brucec@hotmail.com>
2021-06-16 20:46:46 -0700

    Update comments of add_custom_ros_msg.m

--- Integration/add_custom_ros_msg.m
+++ Integration/add_custom_ros_msg.m
@@ -1,5 +1,7 @@
-%Need to delete the previous matlab_gen folder in ~/catkin_ws/src
-%before running this script to generate new messages
+%Where applicable, need to delete the following before running script:
+% 1. ~/catkin_ws/src/matlab_gen
+% 2. ~/catkin_ws/src/boat_controller
 
+%replace with your catkin_ws/src folder path
 folderpath = '/home/bruce/catkin_ws/src/';
 rosgenmsg(folderpath)
