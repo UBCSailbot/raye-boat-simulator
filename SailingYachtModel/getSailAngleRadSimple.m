d9f3d9cc1024c7deaf680503f68c823d239a5ff1
Pidud <Tsuchitorijoel@gmail.com>
2021-06-19 12:16:49 -1000

    added simplified sail controller model and unit tests (#18)
    * added simplified sail controller model and unit tests
    
    * renamed files and variables and added better documentation to getSailAngleRadSimple
    
    * Wrapped Simple Sail input between -Pi and Pi and clarified comments

---
+++ SailingYachtModel/getSailAngleRadSimple.m
@@ -0,0 +1,36 @@
+function [sailAngle] = getSailAngleRadSimple(apparentWindAngleRad)
+%Simplified sail controller, approximates sail angle with basic stepwise
+%function
+%Aprroximates Sail Control with a stepwise function, if angle to wind is
+%below x1 then sets sail angle to maxSailAngle, if the angle is above x2
+%then sets sail angle to minSailAngle, otherwise approximates sail angle
+%using a linear interpolations
+%
+%Method taken from this paper:
+%https://core.ac.uk/download/pdf/79618904.pdf
+
+%lower bound of stepwise function
+x1 = 0.25 * pi;
+%upperbound of stepwise function
+x2 = (0.75) * pi;
+
+%minimum sail angle
+minSailAngle = 0;
+
+%maximum sail Angle
+maxSailAngle = pi/2;
+
+%Bound the Wind Angle between -pi and pi
+apparentWindAngleRad = mod(apparentWindAngleRad - pi, 2 * pi) - pi;
+
+if(abs(apparentWindAngleRad) > x2)
+   sailAngle = minSailAngle; 
+   
+elseif(abs(apparentWindAngleRad) <x1)
+    sailAngle = maxSailAngle;
+else
+    sailAngle = interp1([x1 x2], [maxSailAngle minSailAngle], abs(apparentWindAngleRad));
+end
+
+end
+
