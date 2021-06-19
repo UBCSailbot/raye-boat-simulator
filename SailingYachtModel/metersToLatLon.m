9371fd0758edb4ac28fc158cc1c503d32e87cade
Bruce Cui <34466348+brucecui97@users.noreply.github.com>
2021-06-19 14:13:57 -0700

    Update metersToLatlon to account for x-north y-east in Jouffrey paper (#21)

--- SailingYachtModel/metersToLatLon.m
+++ SailingYachtModel/metersToLatLon.m
@@ -3,7 +3,12 @@ function [lat, lon] = metersToLatLon(x,y)
     %Coordinates are in meter starting in bottom left of squares which
     %divide earth into 60 x 60 grid
     %returns GPS coordinates in decimal degrees
-    utmZone = '17 T';
-    [lat, lon] = utm2deg(x, y, utmZone); 
+    utmZone = '10 N'; %10 N is near Vancouver
+    
+    %in utm2deg coordinate x is east and y is north. In Jouffrey paper, x
+    %is north y is east. so switching the input.
+    
+    %calling y,x because in Jouffrey paper, x is north and y is east
+    [lat, lon] = utm2deg(y, x, utmZone); 
    
 end
--- SailingYachtModel/metersToLatLonTest.m
+++
@@ -1,8 +0,0 @@
-%CN Tower
-testFunction(630084,4833438, 43.6426,-79.3871);
-testFunction(530444 ,2833438 ,25.6182,-80.6968);
-
-function testFunction(x,y, expectedLat, expectedLon)
-    [lat, lon] = metersToLatLon(x,y);
-     assert(round(lat,4) == expectedLat && round(lon, 4) == expectedLon);
-end 
