693a8c595a95853c651d20dd0bf1567adfb80766
Joel Tsuchitori <TsuchitoriJoel@gmail.com>
2021-07-07 14:18:52 -1000

    fixed so wind direction represents direction wind is going not direction wind is coming from

--- SailingYachtModel/getJibeControllerErrorRad.m
+++ SailingYachtModel/getJibeControllerErrorRad.m
@@ -1,15 +1,17 @@
 function [error] = getJibeControllerErrorRad(currentHeading, desiredHeading, apparentWindAngle)
 
 %bound all values between 0 and 2Pi
-apparentWindAngle = mod(apparentWindAngle, 2 * pi);
+%flip wind direction by half a turn so 0 degrees heading relative to wind
+%is upwind
+apparentWindAngle = mod(apparentWindAngle + pi, 2 * pi);
 currentHeading = mod(currentHeading, 2 * pi);
 desiredHeading = mod(desiredHeading, 2 * pi);
 
-%find heading relative to wind
+%find heading relative to wind, where 0 degrees is upwind 
 windRelativeCurrentHeading = mod(currentHeading - apparentWindAngle, 2 * pi);
 windRelativeDesiredHeading = mod(desiredHeading - apparentWindAngle, 2 * pi);
 
-%get error term
+%find error term
 error = windRelativeDesiredHeading - windRelativeCurrentHeading;
 
 end
--- SailingYachtModel/getJibeControllerErrorTest.m
+++ SailingYachtModel/getJibeControllerErrorTest.m
@@ -1,6 +1,6 @@
-assert(getJibeControllerErrorRad(pi /2, - pi / 2, 0) == pi);
-assert(getJibeControllerErrorRad(pi /2, - pi / 2, pi) == -pi);
-assert(getJibeControllerErrorRad((pi /2) + 2 * pi, - pi / 2 + 2 * pi, 0) == pi);
-assert(getJibeControllerErrorRad((pi /2) + 2 * pi, - pi / 2 + 2 * pi, pi) == -pi);
-assert(getJibeControllerErrorRad(pi /100, - pi / 100, 0) == (99/100) * 2 * pi);
-assert(getJibeControllerErrorRad(pi /100, - pi / 100, pi) > (-3/100) * pi && getJibeControllerErrorRad(pi /100, - pi / 100, pi) < 0);
+assert(getJibeControllerErrorRad(pi /2, - pi / 2, 0) == -pi);
+assert(getJibeControllerErrorRad(pi /2, - pi / 2, pi) == pi);
+assert(getJibeControllerErrorRad((pi /2) + 2 * pi, - pi / 2 + 2 * pi, 0) == -pi);
+assert(getJibeControllerErrorRad((pi /2) + 2 * pi, - pi / 2 + 2 * pi, pi) == pi);
+assert(getJibeControllerErrorRad(pi /100, - pi / 100, pi) == (99/100) * 2 * pi);
+assert(getJibeControllerErrorRad(pi /100, - pi / 100, 0) > (-3/100) * pi && getJibeControllerErrorRad(pi /100, - pi / 100, 0) < 0);
