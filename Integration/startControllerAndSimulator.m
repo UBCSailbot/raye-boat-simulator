b93726ce15f58ebacdc0aecf05ee583b537f43c0
bruce <brucec@hotmail.com>
2021-12-02 17:17:11 -0800

    add simulation normal parameter not sure if correct

--- Integration/startControllerAndSimulator.m
+++ Integration/startControllerAndSimulator.m
@@ -1,5 +1,4 @@
-%todo: follow this https://answers.ros.org/question/255008/roslaunch-on-matlab/
-
-%incomplete below
-system("source ~/catkin_ws/devel/setup.sh")
-system("rosrun boat_controller boat_controller_node");
+%note runing this the first time you start simulink afte ropeniangMATLAB
+%takes a hilwe (~ 1.5 minutes for Bruce)
+set_param('boat_simulator_ros_node', 'SimulationMode', 'normal')
+sim boat_simulator_ros_node.slx;
