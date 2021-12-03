5a3904d5c357f2591116dc2a55ce7bfb98ca44a0
bruce <brucec@hotmail.com>
2021-12-02 17:07:19 -0800

    incomplete below

--- Integration/startControllerAndSimulator.m
+++ Integration/startControllerAndSimulator.m
@@ -1,4 +1,5 @@
-%todo: follow this file
+%todo: follow this https://answers.ros.org/question/255008/roslaunch-on-matlab/
 
+%incomplete below
 system("source ~/catkin_ws/devel/setup.sh")
 system("rosrun boat_controller boat_controller_node");
