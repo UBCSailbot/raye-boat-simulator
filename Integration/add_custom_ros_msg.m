47311dbf1ca458011e127f837b2997be335393f2
bruce <brucec@hotmail.com>
2021-12-02 16:46:46 -0800

    add autoclone sailbot message to setup script

--- Integration/add_custom_ros_msg.m
+++ Integration/add_custom_ros_msg.m
@@ -4,9 +4,17 @@
 
 %replace with your catkin_ws/src folder path
 
+
+
 folderpath = "~/catkin_ws/src/";
+%!git clone https://github.com/UBCSailbot/sailbot-msg.git ~/catkin_ws/src/sailbot_msg
+
+system("git clone https://github.com/UBCSailbot/sailbot-msg.git " + folderpath + "sailbot_msg");
+
 rosgenmsg(folderpath)
 addpath(folderpath + "/matlab_gen/msggen")
+
+%todo use ! escape to autotically clone into sailbot msg repo
 
 %todo setup a test to see if can create sailbot message
 %todo also verifiy if updating sailbot message updates atuomatically
