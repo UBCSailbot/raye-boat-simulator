587df100f78ccb29336f3f8f15d55cbb944f3de7
bruce <brucec@hotmail.com>
2021-12-05 15:05:52 -0800

    update add_custom ros msg

--- Integration/add_custom_ros_msg.m
+++ Integration/add_custom_ros_msg.m
@@ -1,7 +1,3 @@
-%Where applicable, need to delete the following before running script:
-% 1. ~/catkin_ws/src/matlab_gen
-% 2. ~/catkin_ws/src/boat_controller
-
 %replace with your catkin_ws/src folder path
 
 catkinSRCFolderpath = "~/catkin_ws/src/";
@@ -11,5 +7,3 @@ rosgenmsg(catkinSRCFolderpath)
 addpath(catkinSRCFolderpath + "/matlab_gen/msggen")
 
 
-%todo setup a test to see if can create sailbot message
-%todo also verifiy if updating sailbot message updates atuomatically
