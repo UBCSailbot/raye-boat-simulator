0929b65d2cbe940af5a6e15aebb71c6693cd0879
bruce <brucec@hotmail.com>
2022-02-27 19:36:09 -0800

    add

--- Integration/add_custom_ros_msg.m
+++ Integration/add_custom_ros_msg.m
@@ -7,6 +7,5 @@ catkinSRCFolderpath = "~/catkin_ws/src/";
 system("git clone https://github.com/UBCSailbot/sailbot-msg.git " + catkinSRCFolderpath + "sailbot_msg");
 
 rosgenmsg(catkinSRCFolderpath)
-addpath(catkinSRCFolderpath + "/matlab_gen/msggen")
 
 
